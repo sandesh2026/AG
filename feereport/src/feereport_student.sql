8ee40bc693477c9ca540b6efc9dc5161407709e5
Sandesh Shetty <shettysandesh.ss1996@gmail.com>
2018-10-31 17:24:41 +0530

    update feereport_student.sql

--- feereport/src/feereport_student.sql
+++ feereport/src/feereport_student.sql
@@ -47,10 +47,10 @@ CREATE TABLE IF NOT EXISTS `feereport_student` (
 -- Dumping data for table `feereport_student`
 --
 
-INSERT INTO `feereport_student` (`rollno`, `name`, `email`, `course`, `fee`, `paid`, `due`, `address`, `city`, `state`, `country`, `contactno`, `registereddate`) VALUES
+/* INSERT INTO `feereport_student` (`rollno`, `name`, `email`, `course`, `fee`, `paid`, `due`, `address`, `city`, `state`, `country`, `contactno`, `registereddate`) VALUES
 (1, 'Prateek Kumar', 'prateekpathak0451@gmail.com', 'Java', 11000, 11000, 0, 'D-23, Sec-18, Noida	', 'Noida', 'UP', 'INDIA', '9329293900', '2016-12-07 09:28:13'),
 (2, 'sallu', 'sallu@gmail.com', 'java', 9000, 9000, 0, 'abc		', 'noida', 'UP', 'India', '32923923', '2016-12-06 09:06:47'),
-(3, 'Mayank', 'mayank@gmail.com', 'Java', 11000, 10000, 1000, '', 'noida', 'UP', 'India', '9329392932', '2016-12-17 08:38:47');
+(3, 'Mayank', 'mayank@gmail.com', 'Java', 11000, 10000, 1000, '', 'noida', 'UP', 'India', '9329392932', '2016-12-17 08:38:47'); */
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
